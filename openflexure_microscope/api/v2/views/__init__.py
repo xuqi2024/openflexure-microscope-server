c46b67f763425a653e3f65b4ad337c35a4d35ca4
Joel Collins <gitlab@jtcollins.net>
2020-12-09 11:17:27 +0000

    Explicit view imports

--- openflexure_microscope/api/v2/views/__init__.py
+++ openflexure_microscope/api/v2/views/__init__.py
@@ -1,6 +1,39 @@
 from .actions import enabled_root_actions
-from .camera import *
-from .captures import *
-from .instrument import *
-from .stage import *
-from .streams import *
+from .camera import LSTImageProperty
+from .captures import (
+    CaptureAnnotations,
+    CaptureDownload,
+    CaptureList,
+    CaptureTags,
+    CaptureView,
+)
+from .instrument import (
+    ConfigurationProperty,
+    NestedConfigurationProperty,
+    NestedSettingsProperty,
+    NestedStateProperty,
+    SettingsProperty,
+    StateProperty,
+)
+from .stage import StageTypeProperty
+from .streams import MjpegFrameState, MjpegStream, SnapshotStream
+
+__all__ = [
+    "enabled_root_actions",
+    "LSTImageProperty",
+    "CaptureList",
+    "CaptureView",
+    "CaptureDownload",
+    "CaptureTags",
+    "CaptureAnnotations",
+    "SettingsProperty",
+    "NestedSettingsProperty",
+    "StateProperty",
+    "NestedStateProperty",
+    "ConfigurationProperty",
+    "NestedConfigurationProperty",
+    "StageTypeProperty",
+    "MjpegStream",
+    "SnapshotStream",
+    "MjpegFrameState",
+]
