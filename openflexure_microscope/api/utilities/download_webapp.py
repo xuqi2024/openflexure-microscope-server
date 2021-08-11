71e4861f98e95cceb8b026468b3942b071e2cf5c
Richard <richard.bowman@cantab.net>
2021-08-11 19:22:53 +0100

    Format fixes

--- openflexure_microscope/api/utilities/download_webapp.py
+++ openflexure_microscope/api/utilities/download_webapp.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import subprocess
 
+
 def download_webapp_from_CI():
     """Download a pre-built archive of the web app.
     
@@ -10,14 +11,20 @@ def download_webapp_from_CI():
     this will save you needing to compile it from source on a Pi too 
     often.
     """
-    parser = argparse.ArgumentParser(description="Download static web app from GitLab CI")
+    parser = argparse.ArgumentParser(
+        description="Download static web app from GitLab CI"
+    )
     parser.add_argument("branch", default="master")
     args = parser.parse_args()
 
     # We want to make sure the command is run at the root of this repository
     # so we take the parent of the current file
-    repo_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
-    if not os.path.isdir(os.path.join(repo_dir, "openflexure_microscope", "api", "static")):
+    repo_dir = os.path.realpath(
+        os.path.join(os.path.dirname(__file__), "..", "..", "..")
+    )
+    if not os.path.isdir(
+        os.path.join(repo_dir, "openflexure_microscope", "api", "static")
+    ):
         print("Couldn't determine the right place to put static files")
         print(f"We think the repository should be at {repo_dir}.")
         exit(-1)
