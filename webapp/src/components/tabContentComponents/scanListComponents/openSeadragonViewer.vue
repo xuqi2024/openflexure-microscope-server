3d69378e2c22ebaca7df38c57936766e4b892b07
Richard Bowman <richard.bowman@cantab.net>
2024-10-11 14:55:40 +0100

    First pass at openseadragon integration
    This adds a very basic openseadragon viewer, currently pointing at files hosted on a web server.

---
+++ webapp/src/components/tabContentComponents/scanListComponents/openSeadragonViewer.vue
@@ -0,0 +1,100 @@
+<template>
+    <div v-observe-visibility="visibilityChanged">
+        <div id="openseadragon">
+            
+        </div>
+        <button class="button" @click="openFullscreen();">Open Fullscreen</button>
+    </div>
+</template>
+
+<script>
+import OpenSeaDragon from "openseadragon";
+
+// Export main app
+export default {
+  name: "OpenSeadragonViewer",
+  components: {  },
+
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  },
+
+  data: function() {
+    return {
+      osdViewer: null
+    };
+  },
+
+  watch: {
+    src: {
+      immediate: true,
+      handler(newVal) {
+        this.loadOpenSeaDragon(newVal);
+      }
+    }
+  },
+
+  async mounted() {
+    if (this.src) {
+      this.loadOpenSeaDragon(this.src);
+    }
+  },
+
+  beforeDestroy() {
+    // Remove global signal listener to perform a gallery refresh
+    this.osdViewer.destroy();
+  },
+
+  methods: {
+    visibilityChanged(isVisible) {
+      if (isVisible) {
+        this.loadOpenSeaDragon();
+      }else{
+        this.osdViewer.destroy();
+      }
+    },
+    async loadOpenSeaDragon() {
+      if (this.osdViewer) {
+        this.osdViewer.destroy();
+      }
+      this.osdViewer = OpenSeaDragon({
+        id: "openseadragon",
+        //prefixUrl: "https://images.openflexure.org/cap_demo/",
+        crossOriginPolicy: 'Anonymous',
+        tileSources: this.src,
+        showNavigationControl: false,
+        maxZoomPixelRatio: 2,
+        gestureSettingsMouse: {
+            clickToZoom: false
+        }
+      });
+    },
+    openFullscreen() {
+      if (this.osdViewer) {
+        this.osdViewer.setFullScreen(true);
+      }
+    },
+  }
+};
+</script>
+
+<style lang="less" scoped>
+#openseadragon {
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  z-index:1;
+}
+#info-panel {
+    position: relative;
+    top: 10px;
+    left: 10px;
+    background-color: rgba(255, 255, 255, 0.7);
+    padding: 5px;
+    border-radius: 1px;
+    z-index: 1000;
+}
+</style>
