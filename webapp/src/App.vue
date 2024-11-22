54fc2bc8c95a87eeb08dc4d6832124ba5433d53a
jaknapper <jaknapper@hotmail.com>
2024-11-22 07:59:23 +0000

    Remove tour from webapp

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -196,7 +196,7 @@ export default {
     this.checkConnection();
     // Handle guided tour
     // If the user has already completed or skipped the guided tour
-    var completedTour = this.getLocalStorageObj("completedTour") || false;
+    var completedTour = this.getLocalStorageObj("completedTour") || true;
     if (!completedTour) {
       this.$tours["guidedTour"].start();
     }
