b1071d85cd6015cf45e19b0f3afae781b47d7a6c
Richard Bowman <richard.bowman@cantab.net>
2024-12-20 14:33:55 +0000

    Add UI and config for proscan

--- webapp/src/components/tabContentComponents/settingsComponents/stageSettings.vue
+++ webapp/src/components/tabContentComponents/settingsComponents/stageSettings.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="stageSettings" class="uk-width-large">
-    The microscope stage is a <b>{{ stageType }}</b
-    >. There are no settings to adjust here: to set the geometry of your stage
-    (i.e. switch between delta and cartesian), you will need to adjust the
-    configuration of your microscope, which cannot be done while it is running.
+    <div>The microscope stage is a <b>{{ stageType }}</b>.</div>
+    <div>
+      <action-button
+        thing="stage"
+        action="query"
+        :submit-data="{ message: 'SMZ 1' }"
+        submit-label="Enable slow Z moves"
+        @error="modalError"
+      />
+    </div>
+    
+    
   </div>
 </template>
 
 <script>
+import ActionButton from "../../labThingsComponents/actionButton.vue";
+
 export default {
   name: "StageSettings",
 
-  components: {},
+  components: {
+    ActionButton
+  },
 
   data: function() {
     return {};
