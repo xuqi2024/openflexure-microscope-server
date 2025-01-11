c37437992b090a537d5abbcc7d4f28312c9c4539
jaknapper <jaknapper@hotmail.com>
2025-01-11 11:38:45 +0000

    Rename exposure (with limit)

--- webapp/src/components/tabContentComponents/settingsComponents/cameraSettings.vue
+++ webapp/src/components/tabContentComponents/settingsComponents/cameraSettings.vue
@@ -13,7 +13,7 @@
                 <a class="uk-accordion-title" href="#">Pi Camera Settings</a>
                 <div class="uk-accordion-content">
                   <PropertyControl
-                    label="Brightness (0-33251)"
+                    label="Exposure time (0-33251)"
                     property-name="exposure_time"
                     thing-name="camera"
                     :read-back-delay="1000"
