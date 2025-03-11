2109cd08667e8f5655bb25aa3f6dcee5c266d801
jaknapper <jaknapper@hotmail.com>
2025-03-11 11:21:12 +0000

    Minimum peak quality in stitching

--- src/openflexure_microscope_server/things/smart_scan.py
+++ src/openflexure_microscope_server/things/smart_scan.py
@@ -1345,6 +1345,8 @@ class SmartScanThing(Thing):
                 "1",
                 "--max_stage_discrepancy",
                 "130",
+                "--min_peak_quality",
+                "0.9995",
                 os.path.join(images_folder, "images", "use"),
             ],
         )
