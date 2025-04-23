6cac0d8cb1696cd5a10d3b12248c156133e5d547
jaknapper <jaknapper@hotmail.com>
2025-04-23 19:16:23 +0100

    Stacking in autofocus with temp capture Thing

--- src/openflexure_microscope_server/things/autofocus.py
+++ src/openflexure_microscope_server/things/autofocus.py
@@ -14,6 +14,9 @@ from typing import Annotated, Mapping, Optional, Sequence
 import os
 import shutil
 import glob
+import os
+import shutil
+import glob
 
 from fastapi import Depends
 import numpy as np
@@ -31,6 +34,9 @@ from .camera import RawCameraDependency as Camera
 from .camera import CameraDependency as WrappedCamera
 from .stage import StageDependency as Stage
 from .capture import CaptureThing
+import numpy as np
+from pydantic import BaseModel
+
 
 CaptureDep = direct_thing_client_dependency(CaptureThing, "/capture/")
 
--- src/openflexure_microscope_server/things/smart_scan.py
+++ src/openflexure_microscope_server/things/smart_scan.py
@@ -32,7 +32,6 @@ from openflexure_microscope_server.utilities import ErrorCapturingThread
 from openflexure_microscope_server.things.autofocus import AutofocusThing
 from openflexure_microscope_server.things.camera_stage_mapping import CameraStageMapper
 from openflexure_microscope_server.things.background_detect import BackgroundDetectThing
-from openflexure_microscope_server.things.z_stack import ZStackThing
 from openflexure_microscope_server import scan_planners
 
 CSMDep = direct_thing_client_dependency(CameraStageMapper, "/camera_stage_mapping/")
@@ -136,7 +135,6 @@ class SmartScanThing(Thing):
         self._background_detect: Optional[BackgroundDep] = None
         self._ongoing_scan_name: Optional[str] = None
         self._starting_position: Optional[Mapping[str, int]] = None
-        self._z_stack: Optional[ZStackDep] = None
         self._capture_thread: Optional[ErrorCapturingThread] = None
         self._scan_images_taken: Optional[int] = None
         # TODO Scan data is a dict during refactoring, should become a dataclass
@@ -153,7 +151,6 @@ class SmartScanThing(Thing):
         metadata_getter: GetThingStates,
         csm: CSMDep,
         background_detect: BackgroundDep,
-        z_stack: ZStackDep,
         scan_name: str = "",
     ):
         """Move the stage to cover an area, taking images that can be tiled together.
@@ -177,7 +174,6 @@ class SmartScanThing(Thing):
         self._csm = csm
         self._background_detect = background_detect
         self._capture_thread = None
-        self._z_stack = z_stack
         self._scan_images_taken = 0
 
         # Don't set self._scan_data dictionary. This is done at the start of _run_scan
@@ -213,7 +209,6 @@ class SmartScanThing(Thing):
             self._ongoing_scan_name = None
             self._scan_images_taken = None
             self._scan_data = None
-            self._z_stack = None
             self._scan_lock.release()
 
     def promote_stitch_files(
