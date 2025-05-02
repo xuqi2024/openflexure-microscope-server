fdd8686cf2c45cc4049e501d1f24c55d90d8fc06
jaknapper <jaknapper@hotmail.com>
2025-05-02 13:40:41 +0100

    Stream_name and capture_method args updated

--- src/openflexure_microscope_server/things/autofocus.py
+++ src/openflexure_microscope_server/things/autofocus.py
@@ -28,7 +28,7 @@ from labthings_fastapi.dependencies.invocation import InvocationLogger
 from .camera import RawCameraDependency as Camera
 from .camera import CameraDependency as WrappedCamera
 from .stage import StageDependency as Stage
-from .capture import CaptureThing, _save_capture
+from .capture import CaptureThing
 import numpy as np
 from pydantic import BaseModel
 
