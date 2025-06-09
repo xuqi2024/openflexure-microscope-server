6d84345f394e168be9671836e9aae547e6742ec7
Chish36 <b.chisholm2002@gmail.com>
2025-06-09 14:12:46 +0100

    Happy with this

--- src/openflexure_microscope_server/things/camera_stage_mapping.py
+++ src/openflexure_microscope_server/things/camera_stage_mapping.py
@@ -228,7 +228,8 @@ class CameraStageMapper(Thing):
 
         This performs two 1d calibrations in x and y, then combines their results.
         """
-        # Y is more likely to fail so calibrates that axis first
+        # Calibrate y-axis first as it is more likely to fail.
+        # The x-y difference is due to the camera aspect ratio, not the stage hardware.
         logger.info("Calibrating Y axis:")
         cal_y: dict = self.calibrate_1d(hw, stage, logger, (0, 1, 0))
         logger.info("Calibrating X axis:")
